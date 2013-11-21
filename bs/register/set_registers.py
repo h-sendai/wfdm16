26d2a5d33362224b020b2cc96fb9c852f434b53d
hiroshi sendai <hiroshi.sendai@kek.jp>
2013-11-21 16:14:23 +0900

    Update register setting.

--- bs/register/set_registers.py
+++ bs/register/set_registers.py
@@ -20,30 +20,34 @@ def main():
     rbcp.set_timeout(1.0)
     ip_address_list = [
         '192.168.10.16',
-        #'192.168.10.17'
+        '192.168.10.17'
     ]
     
     for ip_address in ip_address_list:
-        board_version = get_board_version(ip_address)
+        #board_version = get_board_version(ip_address)
+        board_version = 2
 
-        # reset ADC
+        # reset ADC (soft reset)
         rbcp.write_register_f(ip_address, 0x10,  '>B', 0x0)
         rbcp.write_register_f(ip_address, 0x100, '>B', 0x3c)
         rbcp.write_register_f(ip_address, 0x10,  '>B', 0x1)
         rbcp.write_register_f(ip_address, 0x100, '>B', 0x3c)
 
+        # internal power down mode (do reset)
         rbcp.write_register_f(ip_address, 0x10,  '>B', 0x0)
         rbcp.write_register_f(ip_address, 0x108, '>B', 0x3)
         rbcp.write_register_f(ip_address, 0x10,  '>B', 0x1)
         rbcp.write_register_f(ip_address, 0x108, '>B', 0x3)
         time.sleep(0.1)
 
+        # internal power down mode (do release reset)
         rbcp.write_register_f(ip_address, 0x10,  '>B', 0x0)
         rbcp.write_register_f(ip_address, 0x108, '>B', 0x0)
         rbcp.write_register_f(ip_address, 0x10,  '>B', 0x1)
         rbcp.write_register_f(ip_address, 0x108, '>B', 0x0)
 
         if board_version == 1:
+            # serial outout number of bits (set 12 bits)
             rbcp.write_register_f(ip_address, 0x10,  '>B', 0x0)
             rbcp.write_register_f(ip_address, 0x121, '>B', 0x3)
             rbcp.write_register_f(ip_address, 0x1ff, '>B', 0x1)
@@ -51,8 +55,15 @@ def main():
             rbcp.write_register_f(ip_address, 0x121, '>B', 0x3)
             rbcp.write_register_f(ip_address, 0x1ff, '>B', 0x1)
             
+        # Output format: offset binary
+        rbcp.write_register_f(ip_address, 0x10, '>B', 0x0)
+        rbcp.write_register_f(ip_address, 0x114, '>B', 0x0)
+        rbcp.write_register_f(ip_address, 0x10, '>B', 0x1)
+        rbcp.write_register_f(ip_address, 0x114, '>B', 0x0)
+
+        # fpga adc interface reset
         rbcp.write_register_f(ip_address, 0x5, '>B', 0x1)
         rbcp.write_register_f(ip_address, 0x5, '>B', 0x0)
-
+    
 if __name__ == '__main__':
     main()
